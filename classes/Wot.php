6a4b65fd02fc594b1fd362035d8ba09b24aad872
Janusz Dymidziuk <janusz@dymidziuk.de>
2015-04-02 20:29:59 +0200

    Init commit

---
+++ classes/Wot.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 02.04.2015
+ * Time: 20:25
+ */
+
+namespace wot_api;
+
+
+class Wot {
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 02.04.2015
+ * Time: 20:24
+ */
